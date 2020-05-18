40435e7ddf9ea89814bced9fff96b4a6688e08d1
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-05-18 21:33:07 +0200

    Adding static html from AppCommponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-elements';
+  content = null;
+  constructor() {
+    setTimeout(() => {
+      this.content = "<p>A paragraph!</p>";
+    },1000);
+  }
 }
